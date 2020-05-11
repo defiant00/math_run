4b0a14be4e7ac98bee208bffea9a203277006d1c
Aaron Mady <amady@regionalmanagement.com>
2020-05-11 15:31:20 -0400

    added todo list

--- scripts/status.gd
+++ scripts/status.gd
@@ -1,5 +1,36 @@
 extends Node
 
+# TODO
+#
+# questions
+# slowdown pickup
+# slow-mo at question
+# menu
+# parallax clouds (faster than BG, slower than player)
+# player death
+#	fall
+#	hit wrong answer area
+# effects at fast running and full-speed running
+# interactive backgrounds
+#	breakable pots / physics objects
+#	birds
+# sprites
+#	player
+#		walk
+#		run
+#		jump
+#		fall
+#		roll
+#	background
+#	slowdown pickup
+#	platforms
+#	question platform
+#	menu
+# music
+#	menu
+#	level
+# sounds
+
 const MAX_PLAYER_SPEED = 2000
 
 var playerSpeed = 300
